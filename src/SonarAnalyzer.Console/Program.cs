fb2a6c74b5432ba25ea79cd35db07a7afef0aa1d
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2024-04-24 16:43:24 +0100

    add simple examples of Sonar rules

--- src/SonarAnalyzer.Console/Program.cs
+++ src/SonarAnalyzer.Console/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using SonarAnalyzer.Console;
+
+var names = new List<string> { "Bob", "John", "Sarah", "Brad", "Ben" };
+var configuration = GetConfiguration();
+
+// Find over FirstOrDefault
+var firstNameMatch = names.Find(x => x.StartsWith("b", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine(firstNameMatch);
+
+//recursions should not be infinite
+void RecursiveMethod(int index)
+{
+    if (index > 10)
+    {
+        return;
+    }
+
+    RecursiveMethod(index);
+}
+
+var outputPath = configuration["Settings:OutputPath"] ?? string.Empty;
+var resourceHolder = new ResourceHolder();
+
+resourceHolder.WriteToFile($"{outputPath}\\testFile.txt", "wow, did this work?!? 123");
+
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder();
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+    return builder.Build();
+}
---
+++ src/SonarAnalyzer.Console/ResourceHolder.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace SonarAnalyzer.Console;
+public class ResourceHolder : IDisposable
+{
+    private FileStream? _fs;
+
+    public void OpenResource(string path)
+    {
+        _fs = new FileStream(path, FileMode.Open);
+    }
+
+    public void WriteToFile(string path, string text)
+    {
+        using (_fs = new FileStream(path, FileMode.Create))
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            _fs.Write(bytes, 0, bytes.Length);
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        _fs?.Dispose();
+    }
+}
